a98ebfb38f7d1cd99e1bd8f666d92c3e279cbf80
QYJleo <129179588+QYJleo@users.noreply.github.com>
2024-03-28 18:06:51 +0800

    Update lgzj.js

--- lgzj.js
+++ lgzj.js
@@ -1,3 +1,16 @@
+/*******************************
+
+[rewrite_local]
+
+^https:\/\/footprint-api\.quthing\.com\/vip\/state url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/lgzj.js
+
+[mitm]
+
+hostname = footprint-api.quthing.com
+
+*************************************/
+
+
 var body = JSON.parse($response.body);
 
 body.data.validVip = true,
